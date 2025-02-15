615aeea7e8a4684d0a98a106e24c23824f6dbc8b
kgsensei <jay@kgsensei.dev>
2025-02-15 17:49:48 -0500

    update simple.js to be more scoped

--- simple/simple.js
+++ simple/simple.js
@@ -8,29 +8,29 @@ const simple={
 function gid(i){return document.getElementById(i)}
 function gcn(i){return document.getElementsByClassName(i)}
 function idex(i){return gid(i)!=null}
-Object.prototype.preHTML=function(h){this.innerHTML=h+this.innerHTML}
-Object.prototype.addHTML=function(h){this.innerHTML+=h}
-Object.prototype.setHTML=function(h){this.innerHTML=h}
-Object.prototype.setText=function(h){this.innerText=h}
-Object.prototype.insertBefore=function(h){this.insertAdjacentHTML("beforebegin",h)}
-Object.prototype.insertTop=function(h){this.insertAdjacentHTML("afterbegin",h)}
-Object.prototype.insertBottom=function(h){this.insertAdjacentHTML("beforeend",h)}
-Object.prototype.insertEnd=function(h){this.insertAdjacentHTML("afterend",h)}
-Object.prototype.hide=function(){this.style.display='none'}
-Object.prototype.show=function(d=null){if(d==null){this.style.display='block'}else{this.style.display=d}}
-Object.prototype.width=function(){return this.offsetWidth}
-Object.prototype.height=function(){return this.offsetHeight}
-Object.prototype.left=function(){return this.offsetLeft}
-Object.prototype.top=function(){return this.offsetTop}
-Object.prototype.val=function(n=null){if(n!=null){this.value=n}else{return this.value}}
-Object.prototype.classAdd=function(n){this.classList.add(n)}
-Object.prototype.classRemove=function(n){this.classList.remove(n)}
-Object.prototype.ael=function(e,c){this.addEventListener(e,c)}
-Object.prototype.rel=function(e,c){this.removeEventListener(e,c)}
-Object.prototype.val=function(n=null){if(n!=null){this.value=n}else{return this.value}}
-Object.prototype.parent=function(){return this.parentElement}
-Object.prototype.setAttr=function(k,v){this.setAttribute(k,v)}
-Object.prototype.getAttr=function(k){return this.getAttribute(k)}
+HTMLElement.prototype.preHTML=function(h){this.innerHTML=h+this.innerHTML}
+HTMLElement.prototype.addHTML=function(h){this.innerHTML+=h}
+HTMLElement.prototype.setHTML=function(h){this.innerHTML=h}
+HTMLElement.prototype.setText=function(h){this.innerText=h}
+HTMLElement.prototype.insertBefore=function(h){this.insertAdjacentHTML("beforebegin",h)}
+HTMLElement.prototype.insertTop=function(h){this.insertAdjacentHTML("afterbegin",h)}
+HTMLElement.prototype.insertBottom=function(h){this.insertAdjacentHTML("beforeend",h)}
+HTMLElement.prototype.insertEnd=function(h){this.insertAdjacentHTML("afterend",h)}
+HTMLElement.prototype.hide=function(){this.style.display='none'}
+HTMLElement.prototype.show=function(d=null){if(d==null){this.style.display='block'}else{this.style.display=d}}
+HTMLElement.prototype.width=function(){return this.offsetWidth}
+HTMLElement.prototype.height=function(){return this.offsetHeight}
+HTMLElement.prototype.left=function(){return this.offsetLeft}
+HTMLElement.prototype.top=function(){return this.offsetTop}
+HTMLElement.prototype.val=function(n=null){if(n!=null){this.value=n}else{return this.value}}
+HTMLElement.prototype.classAdd=function(n){this.classList.add(n)}
+HTMLElement.prototype.classRemove=function(n){this.classList.remove(n)}
+HTMLElement.prototype.ael=function(e,c){this.addEventListener(e,c)}
+HTMLElement.prototype.rel=function(e,c){this.removeEventListener(e,c)}
+HTMLElement.prototype.val=function(n=null){if(n!=null){this.value=n}else{return this.value}}
+HTMLElement.prototype.parent=function(){return this.parentElement}
+HTMLElement.prototype.setAttr=function(k,v){this.setAttribute(k,v)}
+HTMLElement.prototype.getAttr=function(k){return this.getAttribute(k)}
 Array.prototype.sample=function(){return this[Math.floor(Math.random()*this.length)]}
 String.prototype.isUpper=function(){if(this.toUpperCase()==this){return true}return false}
 String.prototype.isLower=function(){if(this.toLowerCase()==this){return true}return false}
